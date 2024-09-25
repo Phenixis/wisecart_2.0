3d126023f6df6f4fc972f6f309ff7f04e739e1aa
Maxime Duhamel <max.duh22@gmail.com>
2024-09-25 06:27:41 +0200

    Refactor login input styles

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -56,7 +56,7 @@ export function Login({ mode = 'signin' }: { mode?: 'signin' | 'signup' }) {
                 autoComplete="email"
                 required
                 maxLength={50}
-                className="appearance-none rounded-xl relative block w-full px-3 py-2 border-2 border-neutral placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                className="appearance-none rounded-xl relative block w-full px-3 py-2  border-neutral placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
                 placeholder="Enter your email"
               />
             </div>
@@ -80,7 +80,7 @@ export function Login({ mode = 'signin' }: { mode?: 'signin' | 'signup' }) {
                 required
                 minLength={8}
                 maxLength={100}
-                className="appearance-none rounded-xl relative block w-full px-3 py-2 border-2 border-neutral placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                className="appearance-none rounded-xl relative block w-full px-3 py-2  border-neutral placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
                 placeholder="Enter your password"
               />
             </div>
