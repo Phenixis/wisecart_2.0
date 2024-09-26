cabacb03336ae54ffa7d0a7bcce6eb271a0da130
Maxime Duhamel <max.duh22@gmail.com>
2024-09-26 07:00:57 +0200

    Refactor Stripe integration and remove unused code

--- lib/payments/stripe.ts
+++ lib/payments/stripe.ts
@@ -7,6 +7,7 @@ import {
   updateTeamSubscription,
 } from '@/lib/db/queries';
 
+require("dotenv").config();
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-06-20',
 });
