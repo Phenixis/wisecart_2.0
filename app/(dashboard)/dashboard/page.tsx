d5d542ce51bd5878539740160545aadd449970f3
Maxime Duhamel <max.duh22@gmail.com>
2024-09-30 19:24:34 +0200

    removed team pages from dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,19 +1,12 @@
 import { redirect } from 'next/navigation';
-import { Settings } from './settings';
-import { getTeamForUser, getUser } from '@/lib/db/queries';
+import { getUser } from '@/lib/db/queries';
 
 export default async function SettingsPage() {
   const user = await getUser();
 
   if (!user) {
     redirect('/login');
+  } else {
+    redirect('/dashboard/general');
   }
-
-  const teamData = await getTeamForUser(user.id);
-
-  if (!teamData) {
-    throw new Error('Team not found');
-  }
-
-  return <Settings teamData={teamData} />;
 }
