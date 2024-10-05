493e504bbb14e675204b8ce656235b34a3961431
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 12:15:28 +0200

    Add IngredientLists component

---
+++ components/ingredientLists.tsx
@@ -0,0 +1,7 @@
+export default function IngredientLists() {
+    return (
+        <div className="p-5 spacing-y-4 size-full bg-green-500">
+            <h2 className="text-xl font-bold">Ingredient Lists</h2>
+        </div>
+    )
+}
