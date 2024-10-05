e065194614e9d80966c1f8ca19e66b388c85e7e2
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 12:16:10 +0200

    Add ShoppingListSkeleton and ShoppingListsSkeleton components

---
+++ components/skeletons/shoppingListSkeleton.tsx
@@ -0,0 +1,7 @@
+export default function ShoppingListSkeleton() {
+    return (
+        <div className="p-5 spacing-y-4 size-full bg-red-500">
+            <div className="h-6 w-16 bg-blue-500" />
+        </div>
+    )
+}
