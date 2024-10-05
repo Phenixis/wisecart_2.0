3871fef0741c07dc29a00ac3401279e327390147
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 19:54:59 +0200

    Refactor dashboard component to display the latest shopping list
    - Refactor the import paths for the shopping list component
    - Add suspense fallbacks for better loading experience
    - Add ShoppingListSkeleton and ShoppingListsSkeleton components
    - Update the layout and remove unnecessary elements

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -1,29 +1,38 @@
-import ShoppingLists from './shoppingLists';
+import ShoppingList from "./ui/shoppingList";
 import MealList from './mealLists';
 import IngredientLists from './ingredientLists';
 import { Suspense } from "react";
 import DashboardSkeleton from "./skeletons/dashboardSkeleton";
+import { getLastShoppingList } from "@/app/dashboard/actions";
+import { getUser } from "@/lib/db/queries";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const user = await getUser();
+    if (!user) {
+        redirect('/sign-in');
+    }
+
+    const shoppingList = await getLastShoppingList(user);
     return (
-        <Suspense fallback={<DashboardSkeleton />}>
-            <div className="p-6 spacing-y-4">
-                <h1 className="text-2xl font-bold">Dashboard</h1>
-                <h2 className="text-xl font-bold">Last Shopping List :</h2>
-                <div className="w-full flex justify-center">
-                    <ShoppingLists />
+        <div className="p-6 spacing-y-4">
+            {/* <h1 className="text-2xl font-bold">Dashboard</h1> */}
+            <h2 className="text-xl font-bold">Latest Shopping List :</h2>
+            <div className="w-full flex justify-center">
+            {shoppingList.map((list) => (
+                <ShoppingList key={list.id} user={user} shoppingList={list} />
+            ))}
+            </div>
+            <div className="flex">
+                <div className="w-1/2">
+                    <h2 className="text-xl font-bold">Meal Lists</h2>
+                    <MealList />
                 </div>
-                <div className="flex">
-                    <div className="w-1/2">
-                        <h2 className="text-xl font-bold">Meal Lists</h2>
-                        <MealList />
-                    </div>
-                    <div className="w-1/2">
-                        <h2 className="text-xl font-bold">Ingredient Lists</h2>
-                        <IngredientLists />
-                    </div>
+                <div className="w-1/2">
+                    <h2 className="text-xl font-bold">Ingredient Lists</h2>
+                    <IngredientLists />
                 </div>
             </div>
-        </Suspense>
+        </div>
     )
 }
