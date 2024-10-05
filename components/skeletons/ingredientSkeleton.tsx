b15a0b67d4d9133d2d82cb8d405bda217be491e5
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 19:56:30 +0200

    Refactor ingredientSkeleton component to improve layout and styling

--- components/skeletons/ingredientSkeleton.tsx
+++ components/skeletons/ingredientSkeleton.tsx
@@ -1,7 +1,8 @@
 export default function ingredientSkeleton() {
     return (
-        <div className="p-5 spacing-y-4 size-full bg-red-500">
-            <div className="h-6 w-16 bg-blue-500" />
+        <div className="w-full h-4 flex items-center">
+            <button className={`p-2 my-1 size-1 border border-neutral rounded-full bg-gray-200`} />
+            <div className="w-full h-4 bg-gray-200 ml-1"/>
         </div>
     )
 }
