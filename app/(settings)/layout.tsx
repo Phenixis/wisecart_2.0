f023cbce0cd8c7ab6c906533818ae4d569563922
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 09:14:14 +0200

    Refactor layout.tsx to include a footer component

--- app/(settings)/layout.tsx
+++ app/(settings)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer';
 import Header from '@/components/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -5,6 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <section className="flex flex-col min-h-screen">
       <Header />
       {children}
+      <Footer />
     </section>
   );
 }
