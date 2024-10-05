85eb1f10d56990f1645a78e339786bff0bc6eb92
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 19:56:45 +0200

    Refactor mealSkeleton and shoppingListSkeleton components to improve layout and styling

--- components/skeletons/mealSkeleton.tsx
+++ components/skeletons/mealSkeleton.tsx
@@ -1,7 +1,18 @@
+import { User as UserIcon } from "lucide-react";
+import IngredientSkeleton from "../skeletons/ingredientSkeleton";
+
 export default function mealSkeleton() {
     return (
-        <div className="p-5 spacing-y-4 size-full bg-red-500">
-            <div className="h-6 w-16 bg-blue-500" />
+        <div className="w-full py-2 space-y-2">
+            <div className="flex items-center">
+                <h4 className="w-1/3 h-6 bg-gray-200"/>
+                <div className="w-3 h-4 bg-gray-200 ml-1"/>
+                <UserIcon size={16}/>
+            </div>
+            <IngredientSkeleton />
+            <IngredientSkeleton />
+            <IngredientSkeleton />
+            <IngredientSkeleton />
         </div>
     )
 }
--- components/skeletons/shoppingListSkeleton.tsx
+++ components/skeletons/shoppingListSkeleton.tsx
@@ -1,7 +1,12 @@
+import MealSkeleton from "./mealSkeleton";
+
 export default function ShoppingListSkeleton() {
     return (
-        <div className="p-5 spacing-y-4 size-full bg-red-500">
-            <div className="h-6 w-16 bg-blue-500" />
+        <div className="p-6 w-64 shadow bg-gray-100 animate-pulse h-[50vh]">
+            <div className="bg-gray-200 w-full h-8"/>
+            <MealSkeleton />
+            <MealSkeleton />
+            <MealSkeleton />
         </div>
     )
 }
