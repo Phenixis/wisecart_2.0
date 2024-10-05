fe69b88fec5966e48bfbb46e774fb3215e3132c8
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 09:15:28 +0200

    Some changes in dashboard's page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,19 @@
 import { redirect } from "next/navigation";
-import { getUser, getTeamSubscriptionStatus } from "@/lib/db/queries";
+import { getUser } from "@/lib/db/queries";
 import Dashboard from "@/components/dashboard";
-import Redirection from "@/components/redirection";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function DashboardPage() {
     const user = await getUser();
     if (!user) {
         redirect('/sign-in');
     }
-    const subscriptionStatus = await getTeamSubscriptionStatus(user.id);
-
+    
     return (
-        (subscriptionStatus === 'active' || subscriptionStatus === 'trialing') ? <Dashboard /> :  Redirection({destination: '/pricing', message: "You need to subscribe to access the dashboard. You'll be redirected in a few seconds."})
+        <Dashboard />
     );
 };
