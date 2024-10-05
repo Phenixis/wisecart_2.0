849f6178cf8f6c8a94e088a7b4b609501f76ed1f
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 12:14:16 +0200

    Refactor file structure for pricingCard component

--- app/(settings)/pricing/page.tsx
+++ app/(settings)/pricing/page.tsx
@@ -1,5 +1,5 @@
 import { getStripePrices, getStripeProducts } from '@/lib/payments/stripe';
-import PricingCard from '@/components/pricingCard';
+import PricingCard from '@/components/ui/pricingCard';
 
 // Prices are fresh for one hour max
 export const revalidate = 3600;
