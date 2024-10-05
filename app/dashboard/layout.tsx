2de0832f250315040d945ed0c99d9d7b61bc577a
Maxime Duhamel <max.duh22@gmail.com>
2024-10-05 19:54:45 +0200

    Refactor layout component and add experimental feature

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,6 +8,7 @@ import { usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import {Menu, X, Home, ShoppingCart, CookingPot, Salad } from 'lucide-react';
 
+export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
@@ -25,45 +26,46 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Header />
       <main className="flex flex-col min-h-[calc(100dvh-68px)] max-w-7xl mx-auto w-full">
         {/* Mobile header */}
-        <div className="lg:hidden flex items-center bg-white border-b border-gray-200 p-4">
-          <Button
-            className="-mr-3"
-            variant="ghost"
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-          >
-            <Menu className="h-6 w-6" />
-            <span className="sr-only">Open sidebar</span>
-          </Button>
-          <div className="flex items-center">
-            <span className="font-medium">Menu</span>
-          </div>
+        <div className="lg:hidden bg-base-100 border-b border-gray-200 p-4">
+            <div
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              className='flex items-center w-fit'>
+              <Button
+                className="-mr-3"
+                variant="ghost"
+                >
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Open sidebar</span>
+              </Button>
+              <div className="flex items-center">
+                <span className="font-medium">Menu</span>
+              </div>
+            </div>
         </div>
 
         <div className="flex flex-1 overflow-hidden h-full">
           {/* Sidebar */}
           <aside
-            className={`w-64 bg-white lg:bg-gray-50 border-r border-gray-200 lg:block ${
+            className={`w-64 border-r border-gray-200 lg:block ${
               isSidebarOpen ? 'block' : 'hidden'
             } lg:relative absolute inset-y-0 left-0 z-40 transform transition-transform duration-300 ease-in-out lg:translate-x-0 ${
               isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
             }`}
           >
-            <nav className="h-full overflow-y-auto p-4">
+            <nav className="h-full overflow-y-auto">
               <Button
-                className="-mr-3 lg:hidden"
+                className="lg:hidden"
                 variant="ghost"
+                size={'icon'}
                 onClick={() => setIsSidebarOpen(!isSidebarOpen)}
               >
-                <X className="h-6 w-6" />
+                <X className="size-6" />
                 <span className="sr-only">Close sidebar</span>
               </Button>
               {navItems.map((item) => (
                 <Link key={item.href} href={item.href} passHref>
                   <Button
-                    variant={pathname === item.href ? 'secondary' : 'ghost'}
-                    className={`my-1 w-full justify-start rounded-xl ${
-                      pathname === item.href ? 'bg-gray-100' : ''
-                    }`}
+                    className={`w-full justify-start bg-base-100 p-5 ${ pathname === item.href ? 'text-primary' : ''}`}
                     onClick={() => setIsSidebarOpen(false)}
                   >
                     <item.icon className="mr-2 h-4 w-4" />
