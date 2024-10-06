9db8f353cbc663bd4556e3a6417f604733482527
Maxime Duhamel <max.duh22@gmail.com>
2024-10-06 11:24:18 +0200

    Create MealListskeleton

---
+++ components/skeletons/mealListsSkeleton.tsx
@@ -0,0 +1,12 @@
+import MealSkeleton from "./mealSkeleton";
+
+export default function MealListsSkeleton() {
+    return (
+        <div className="p-6 w-64 shadow bg-gray-100 animate-pulse h-[50vh]">
+            <div className="bg-gray-200 w-full h-8"/>
+            <MealSkeleton />
+            <MealSkeleton />
+            <MealSkeleton />
+        </div>
+    )
+}
