6aab0c7645adb6c30dbac71601856a310c019d05
Maxime Duhamel <max.duh22@gmail.com>
2024-10-06 12:42:00 +0200

    Refactor layout component to add justify-between class for improved spacing

--- app/(settings)/layout.tsx
+++ app/(settings)/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="flex flex-col min-h-screen">
+    <section className="flex flex-col min-h-screen justify-between">
       <Header />
       {children}
       <Footer />
