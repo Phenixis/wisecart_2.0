d4373c9fe4e46229efbc0078bfe43eb6c65afd2f
Maxime Duhamel <max.duh22@gmail.com>
2024-10-07 19:16:00 +0200

    Refactor ingredient component to support different display modes

--- components/ingredient.tsx
+++ components/ingredient.tsx
@@ -1,12 +1,10 @@
 import IngredientToDoItem from "./ui/ingredientToDoItem";
 import IngredientCard from "./ui/ingredientCard";
 
-export default function Ingredient({ingredient, mode} : {ingredient: any, mode: string}) {
+export default function Ingredient({ingredient, mode} : {ingredient: any, mode?: string}) {
     switch (mode) {
         case 'card':
             return <IngredientCard ingredient={ingredient} />;
-        case 'list':
-            return <IngredientToDoItem ingredient={ingredient} />;
         default:
             return <IngredientToDoItem ingredient={ingredient} />;
     }
