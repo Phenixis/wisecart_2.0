165ad622fe59e78eff6d810cf32d6d83074d48e8
Maxime Duhamel <max.duh22@gmail.com>
2024-10-08 06:32:31 +0200

    Refactor ingredientEditPopup component to add a new ingredient form

---
+++ components/ui/ingredientEditPopup.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { Loader2, Pen, Plus } from 'lucide-react';
+import { useState, useActionState, useEffect } from 'react';
+import { Button } from './button';
+import { Input } from './input';
+import { ActionState } from '@/lib/auth/middleware';
+import { createIngredient } from '@/app/dashboard/actions';
+
+export default function EditPopup({ ingredient } : {ingredient: any}) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [state, formAction, pending] = useActionState<ActionState, FormData>(
+        createIngredient,
+        { error: '' }
+    );
+    
+    useEffect(() => {
+        const handleEsc = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                setIsOpen(false);
+            }
+        };
+        
+        document.addEventListener('keydown', handleEsc);
+        
+        return () => {
+            document.removeEventListener('keydown', handleEsc);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (state?.success) {
+            setIsOpen(false);
+        }
+    }, [state]);
+
+    return (
+        <>
+            <button onClick={() => setIsOpen(true)}>
+                <Pen size={16} className="text-primary"/>
+            </button>
+            {isOpen ? (
+            <div 
+                className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center" 
+                onClick={() => setIsOpen(false)}
+            >
+                <form 
+                    className="bg-white rounded-xl p-4 w-96 space-y-4" 
+                    onClick={(e) => e.stopPropagation()}
+                    action={formAction}
+                >
+                    <h2 className="text-2xl font-semibold">Create a new Ingredient</h2>
+                    <div>
+                        <label className="block text-sm font-semibold">Name</label>
+                        <Input 
+                            id="name"
+                            name="name"
+                            type="text"
+                            className="appearance-none rounded-xl relative block w-full px-3 py-2  border-neutral placeholder:italic placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-primary focus:border-primary focus:z-10 sm:text-sm"
+                            placeholder="Pineapple"
+                            value={ingredient.name}
+                            required
+                        />
+                    </div>
+                    {state?.error && (
+                        <div className="text-red-500 text-sm">{state.error}</div>
+                    )}
+                    <Button 
+                        type="submit"
+                        className="rounded-xl text-white font-semibold"
+                        disabled={pending}
+                    >
+                        {pending ? (
+                            <>
+                                <Loader2 className="animate-spin mr-2 h-4 w-4" />
+                                Loading...
+                            </>
+                        ) : (
+                            <>
+                                <Plus size={16} className="mr-2" />
+                                New Ingredient
+                            </>
+                        )}
+                    </Button>
+                    {/* TODO: ADD DELETE BUTTON */}
+                </form>
+            </div>
+            ) : ''}
+        </>
+    )
+}
