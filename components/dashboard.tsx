70a0c9e384d4fa6be5115ce1d4d419fd689f6b1f
Maxime Duhamel <max.duh22@gmail.com>
2024-10-09 06:36:23 +0200

    Add link to pages

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -4,6 +4,8 @@ import IngredientLists from './ingredientLists';
 import { getLastShoppingList } from "@/app/dashboard/actions";
 import { getUser } from "@/lib/db/queries";
 import { redirect } from "next/navigation";
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
 
 export default async function Dashboard() {
     const user = await getUser();
@@ -15,8 +17,11 @@ export default async function Dashboard() {
 
     return (
         <div className="p-6 spacing-y-4">
-            {/* <h1 className="text-2xl font-bold">Dashboard</h1> */}
-            <h2 className="text-xl font-bold">Latest Shopping List :</h2>
+            <h1 className="hidden text-2xl font-bold">Dashboard</h1>
+            <Link key={"/dashboard/shopping-lists"} href={"/dashboard/shopping-lists"} passHref className="flex group">
+                <h2 className="text-xl font-bold">Latest shopping list</h2>
+                <ArrowUpRight size={16} className="hidden text-primary group-hover:block" />
+            </Link>
             <div className="w-full flex justify-center">
             {shoppingList.map((list) => (
                 <ShoppingList key={list.id} user={user} shoppingList={list} />
@@ -24,12 +29,18 @@ export default async function Dashboard() {
             </div>
             <div className="flex">
                 <div className="w-1/2">
-                    <h2 className="text-xl font-bold">Meal Lists</h2>
+                <Link key={"/dashboard/meal-list"} href={"/dashboard/meal-list"} passHref className="flex group">
+                        <h2 className="text-xl font-bold">Meal List</h2>
+                        <ArrowUpRight size={16} className="hidden text-primary group-hover:block" />
+                    </Link>
                     <MealList />
                 </div>
                 <div className="w-1/2">
-                    <h2 className="text-xl font-bold">Ingredient Lists</h2>
-                    <IngredientLists />
+                    <Link key={"/dashboard/ingredient-list"} href={"/dashboard/ingredient-list"} passHref className="flex group">
+                        <h2 className="text-xl font-bold">Ingredient List</h2>
+                        <ArrowUpRight size={16} className="hidden text-primary group-hover:block" />
+                    </Link>
+                    <IngredientLists isCreationDateVisible={false} isLastUpdateDateVisible={false} isEditPossible={false}/>
                 </div>
             </div>
         </div>
