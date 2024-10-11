8582272ae1af0044e146678e0a2204c3fbd7f7f8
Maxime Duhamel <max.duh22@gmail.com>
2024-10-11 06:29:40 +0200

    Refactor MealLists component to include user authentication and display meals

--- components/mealLists.tsx
+++ components/mealLists.tsx
@@ -1,11 +1,24 @@
 import { Suspense } from "react";
 import MealListsSkeleton from "./skeletons/mealListsSkeleton";
+import { getAllMeals } from "@/app/dashboard/actions";
+import { getUser } from "@/lib/db/queries";
+import { redirect } from "next/navigation";
+import Meal from "./ui/meal";
 
-export default function MealLists() {
+export default async function MealLists() {
+    const user = await getUser();
+    if (!user) {
+        redirect('/sign-in');
+    }
+
+    const meals = await getAllMeals(user);
+
     return (
         <Suspense fallback={<MealListsSkeleton />}>
-            <div className="p-5 spacing-y-4 size-full bg-purple-500">
-                <h2 className="text-xl font-bold">Meal Lists</h2>
+            <div className="p-4 flex items-center flex-wrap space-x-4">
+                {meals.map((meal) => (
+                    <Meal key={meal.id} mode={"card"} user={user} meal={meal} />
+                ))}
             </div>
         </Suspense>
     )
