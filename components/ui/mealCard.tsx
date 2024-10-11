92f03418bbed5f62d0a89d826bc2be0bdd838af4
Maxime Duhamel <max.duh22@gmail.com>
2024-10-11 06:30:36 +0200

    Refactor MealCard component to include ingredients and user authentication

---
+++ components/ui/mealCard.tsx
@@ -0,0 +1,31 @@
+import { getIngredientsOfMeal } from "@/app/dashboard/actions";
+import { Meal as MealType, User } from "@/lib/db/schema";
+import Ingredient from "./ingredient";
+import { User as UserIcon } from "lucide-react";
+import { Suspense } from "react";
+import MealSkeleton from "./../skeletons/mealSkeleton";
+
+export default async function MealCard({ user, meal }: { user: User, meal: any /* MealType + mealOrder */}) {
+
+    const ingredients = await getIngredientsOfMeal(user, meal.id);
+
+    return (
+        <Suspense fallback={<MealSkeleton />}>
+            <div className="bg-white shadow rounded-xl w-fit p-2 mb-4">
+                <div className="flex items-center">
+                    <h4 className="text-xl leading-none">{meal.name}</h4>
+                    <p className="text-sm ml-2">
+                        {meal.nbPersons}
+                    </p>
+                    <UserIcon size={16}/>
+                </div>
+                <p className="text-sm text-gray-500">{meal.description}</p>
+                <div>
+                    {ingredients.map((ingredient) => (
+                        <Ingredient key={ingredient.id} ingredient={ingredient} mode={"mealCard"} mealId={meal.id}/>
+                    ))}
+                </div>
+            </div>
+        </Suspense>
+    )
+}
