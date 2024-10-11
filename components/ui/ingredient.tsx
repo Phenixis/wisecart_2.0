4c429e3cd3a357988ca23c7b7041c3ef24d7e799
Maxime Duhamel <max.duh22@gmail.com>
2024-10-11 06:29:44 +0200

    Refactor Ingredient component to include IngredientMealCard

--- components/ui/ingredient.tsx
+++ components/ui/ingredient.tsx
@@ -1,11 +1,14 @@
 import IngredientToDoItem from "./ingredientToDoItem";
 import IngredientCard from "./ingredientCard";
 import { isIngredientToggled as isIngredientToggledFunction } from "@/app/dashboard/actions";
+import IngredientMealCard from "./ingredientMealCard";
 
 export default async function Ingredient({ingredient, mode, isCreationDateVisible, isLastUpdateDateVisible, isEditPossible, mealId, shoppingListId, mealOrder} : {ingredient: any, mode?: string, isCreationDateVisible?: boolean, isLastUpdateDateVisible?: boolean, isEditPossible?: boolean, mealId?: number, shoppingListId?: number, mealOrder?: number}) {
     switch (mode) {
         case 'card':
             return <IngredientCard ingredient={ingredient} isCreationDateVisible={isCreationDateVisible} isLastUpdateDateVisible={isLastUpdateDateVisible} isEditPossible={isEditPossible}/>;
+        case 'mealCard':
+            return <IngredientMealCard ingredient={ingredient}/>;
         default:
             if (mealId === undefined) {
                 throw new Error('Meal ID is required');
